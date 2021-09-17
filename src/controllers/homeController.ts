2aab6d77cd2ff5b6fbb758c736fa366f131340dd
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2021-09-17 10:27:24 -0300

    Estabelecendo conexão e recebendo dados da tabela users

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,24 +1,14 @@
 import { Request, Response } from 'express';
 
 import { Product } from '../models/Product';
+import { User } from '../models/User';
 
-export const home = (req: Request, res: Response)=>{
-    let age: number = 90;
-    let showOld: boolean = false;
+export const home = async (req: Request, res: Response) => {
 
-    if(age > 50) {
-        showOld = true;
-    }
-
-    let list = Product.getAll();
-    let expensiveList = Product.getFromPriceAfter(12);
-
+    let users = await User.findAll();
     res.render('pages/home', {
-        name: 'Bonieky',
-        lastName: 'Lacerda',
-        showOld,
-        products: list,
-        expensives: expensiveList,
-        frasesDoDia: []
+        users
     });
+
 };
+
