4a350ca1abb8df34b9db8902a0520a6fe5f48f1c
Tatu-Armadillo <prohazor.fa@gmail.com>
2021-09-18 15:51:31 -0300

    Insert

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -5,15 +5,24 @@ import { User } from '../models/User';
 
 export const home = async (req: Request, res: Response) => {
 
+    let filtro: string = req.query.name as string;
+    if (!filtro) {
+        filtro = '%%%';
+    }
     let users = await User.findAll({
         where: {
-            [Op.or]: [
-                {age: 20},
-                {name: 'Julia'}
-            ]
-        }
-        // where: {age: 20},
-        // attributes: ['name', 'age']
+            age: {
+                [Op.gte]: filtro
+            }
+        },
+        // offset: 2, // Pula de X em X
+        limit: 10 // Limitar duas linhas
+        // Decidir a ordenação e a sequencia da ordenação
+        // order: [
+        //     ['name', 'ASC'],
+        //     ['age', 'DESC']
+        // ]
+
     });
     res.render('pages/home', {
         users
@@ -21,3 +30,19 @@ export const home = async (req: Request, res: Response) => {
 
 };
 
+export const novoUsuario = async (req: Request, res: Response) => {
+    let { name, age } = req.body
+    if (name && age) {
+        console.log('Primeiro if');
+        const user = await User.create({
+            name: name,
+            age: age
+        });
+    } else if (name && !age) {
+        console.log('segundo if');
+        const user = await User.create({
+            name: name,
+        });
+    }
+    res.redirect('/');
+}
